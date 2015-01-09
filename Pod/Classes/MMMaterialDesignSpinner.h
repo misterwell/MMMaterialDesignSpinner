761621d049e04181987b941248c3efdf1d928a17
Mike Maxwell <misterwell@gmail.com>
2015-01-08 23:54:59 -0800

    #2 - add comments to public header file

--- Pod/Classes/MMMaterialDesignSpinner.h
+++ Pod/Classes/MMMaterialDesignSpinner.h
@@ -8,14 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  A control similar to iOS' UIActivityIndicatorView modeled after Google's Material Design Activity spinner.
+ */
 @interface MMMaterialDesignSpinner : UIView
 
+/** Sets the line width of the spinner's circle. */
 @property (nonatomic) CGFloat lineWidth;
+
+/** Sets whether the view is hidden when not animating. */
 @property (nonatomic) BOOL hidesWhenStopped;
+
+/** Specifies the timing function to use for the control's animation. Defaults to kCAMediaTimingFunctionEaseInEaseOut */
 @property (nonatomic, strong) CAMediaTimingFunction *timingFunction;
+
+/** Property indicating whether the view is currently animating. */
 @property (nonatomic, readonly) BOOL isAnimating;
 
+
+/**
+ *  Starts animation of the spinner.
+ */
 - (void)startAnimating;
+
+/**
+ *  Stops animation of the spinnner.
+ */
 - (void)stopAnimating;
 
 @end
